0af02c6d1db951e9c42e9c5947e03b41e421e05d
zaki <natourzaki@gmail.com>
2021-12-25 18:03:16 +0700

    shorten the reduce function to find the max and min mark

--- dist/index.js
+++ dist/index.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const data_1 = require("./data");
 function getStatistics(marks) {
     const avgMark = marks.reduce((prv, cur) => prv + cur.avgMark, 0) / marks.length;
-    const highestMark = marks.reduce((prv, cur) => { return (prv.avgMark > cur.avgMark) ? prv : cur; }).name;
-    const lowestMark = marks.reduce((prv, cur) => { return (prv.avgMark < cur.avgMark) ? prv : cur; }).name;
+    const highestMark = marks.reduce((prv, cur) => (prv.avgMark > cur.avgMark) ? prv : cur).name;
+    const lowestMark = marks.reduce((prv, cur) => (prv.avgMark < cur.avgMark) ? prv : cur).name;
     const resObj = {
         avgMark,
         highestMark,
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { testMarks } from './data'
 
 function getStatistics(marks: Student[]): Statistics{
     const avgMark=marks.reduce((prv, cur)=>prv+cur.avgMark,0)/marks.length;
-    const highestMark=marks.reduce((prv, cur)=>{return (prv.avgMark > cur.avgMark)? prv: cur}).name;
-    const lowestMark=marks.reduce((prv, cur)=>{return (prv.avgMark < cur.avgMark)? prv: cur}).name;
+    const highestMark=marks.reduce((prv, cur)=> (prv.avgMark > cur.avgMark)? prv: cur).name;
+    const lowestMark=marks.reduce((prv, cur)=> (prv.avgMark < cur.avgMark)? prv: cur).name;
     const resObj={
         avgMark,
         highestMark,
